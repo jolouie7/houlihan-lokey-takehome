46728ac19d8bd522ea38401f7da1f1a1b8f8f3f0
Joseph <joseph161216@gmail.com>
2023-06-01 17:47:28 -0700

    Add custom fonts

---
+++ app.js
@@ -0,0 +1,29 @@
+let Font = Quill.import("formats/font");
+Font.whitelist = ["arial", "roboto"];
+Quill.register(Font, true);
+
+const toolbarOptions = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  ["bold", "italic", "underline"],
+  [{ font: ["", "arial", "roboto"] }],
+  [{ size: ["small", false, "large", "huge"] }],
+  [{ align: [] }],
+  ["link", "image", "code-block"],
+];
+
+const editor = new Quill("#editor", {
+  modules: {
+    toolbar: toolbarOptions,
+  },
+  theme: "snow",
+});
+
+const readonlyEditor = new Quill("#readonly-editor", {
+  theme: "bubble",
+  readOnly: true,
+});
+
+editor.on("text-change", () => {
+  const content = editor.getContents();
+  readonlyEditor.setContents(content);
+});

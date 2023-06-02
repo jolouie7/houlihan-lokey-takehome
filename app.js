d73a2dca292fbcf13bbbb79314de8dc092d7b295
Joseph <joseph161216@gmail.com>
2023-06-02 15:02:53 -0700

    Add onClick event handler

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Register custom fonts
 let Font = Quill.import("formats/font");
 Font.whitelist = ["arial", "roboto", "monospace"];
 Quill.register(Font, true);
 
+// Register custom font sizes
 var Size = Quill.import("attributors/style/size");
 Size.whitelist = ["12px", "14px", "16px", "18px"];
 Quill.register(Size, true);
@@ -15,19 +17,49 @@ const toolbarOptions = [
   ["link", "image", "code-block"],
 ];
 
-const editor = new Quill("#editor", {
-  modules: {
-    toolbar: toolbarOptions,
-  },
-  theme: "snow",
-});
+try {
+  const editor = new Quill("#editor", {
+    modules: {
+      toolbar: toolbarOptions,
+    },
+    theme: "snow",
+  });
 
-const readonlyEditor = new Quill("#readonly-editor", {
-  theme: "bubble",
-  readOnly: true,
-});
+  const readonlyEditor = new Quill("#readonly-editor", {
+    theme: "bubble",
+    readOnly: true,
+  });
 
-editor.on("text-change", () => {
-  const content = editor.getContents();
-  readonlyEditor.setContents(content);
-});
+  const submitButton = document.querySelector(".submit-button");
+  if (!submitButton) {
+    throw new Error("Submit button not found");
+  }
+
+  submitButton.addEventListener("click", () => {
+    const editorElement = document.querySelector("#editor"); // Quill editor DOM element
+    if (!editorElement) {
+      throw new Error("Editor element not found");
+    }
+    const canvas = document.querySelector("#readonly-editor"); // Your canvas DOM element
+    if (!canvas) {
+      throw new Error("Canvas element not found");
+    }
+
+    html2canvas(editorElement)
+      .then((canvasContent) => {
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+
+        // draw the editor content on the canvas
+        ctx.drawImage(canvasContent, 0, 0);
+
+        // clear the editor content
+        editor.setText("");
+      })
+      .catch((error) => {
+        console.error("Error occurred during canvas conversion: ", error);
+      });
+  });
+} catch (error) {
+  console.error("Error occurred while setting up the editor: ", error);
+}
